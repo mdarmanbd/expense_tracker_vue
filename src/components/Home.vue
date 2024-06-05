b48bc94e17ed580a411c1e6a78b6d43518a10bca
arman_hossain <md.armantext@gmail.com>
2024-06-05 23:30:29 +0600

    complete the functionality

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,13 +1,5 @@
 <script setup>
-import { ref, reactive, watch } from 'vue';
-
-// const incomeHistory = reactive([
-//     {
-//     id:0,
-//     name: 'food',
-//     amount: 20,
-//     },
-// ]);
+import { ref, reactive, watch, computed } from 'vue';
 
 
 const addItemParse = JSON.parse(localStorage.getItem("addItem")) || []
@@ -15,6 +7,7 @@ const addItem = reactive(addItemParse);
 const itemName = ref('')
 const itemAmount = ref()
 const balance = ref(100)
+
 
 const submitBtn = () => {
    if(itemName.value === '' || itemAmount.value === ''){
@@ -24,8 +17,7 @@ const submitBtn = () => {
             id: addItem.length + 1 ,
             name : itemName.value,
             amount : itemAmount.value
-        })
-            
+        }) 
         localStorage.setItem("addItem", JSON.stringify(addItem))
         itemName.value = ''
         itemAmount.value = ''
@@ -33,22 +25,37 @@ const submitBtn = () => {
     
 }
 
-const income = () => {
-    let incomeAmout = 0
-    for(let i = 0 ; i<addItem.length; i++){
-       const amount = addItem[i].amount
-       incomeAmout+=amount
+
+const positiveNumber = computed (()=>{
+    let total = 0
+    for(let item of addItem){
+        if(item.amount > 0){
+            total = total + item.amount
+        }
     }
+    return total
+})
 
-    return incomeAmout
-}
+const negativeNumber = computed(()=>{
+    let total = 0 
+    for(let item of addItem){
+        if(item.amount<0){
+            total+=item.amount
+        } 
+    }
+    return total
+})
 
-const expense = () => {
+const totalBalnce = computed(() => {
+   return balance.value + positiveNumber.value + negativeNumber.value
+})
 
-}
-
-const totalBalnce = () => {
-   return balance.value + income()
+const removeItem = (item) => {
+    let i = addItem.indexOf(item)
+    
+    const remove = addItem.splice(i, 1)
+    localStorage.setItem("addItem", JSON.stringify(addItem))
+    return remove
 }
 
 // watch (addItem, (value) => {
@@ -65,24 +72,25 @@ const totalBalnce = () => {
         <div class="w-[500px] m-auto bg-white py-3 px-5">
             <h4 class="text-base font-medium text-gray-800 ">Expense Tracker</h4>
             <h6 class="text-sm font-normal text-gray-800 mt-2">YOUR BALANCE</h6>
-            <h3 class="text-base font-medium text-gray-800 ">$ {{ totalBalnce() }}</h3>
+            <h3 class="text-base font-medium text-gray-800 ">$ {{ totalBalnce }}</h3>
             <div class="w-full grid grid-cols-2 gap-3 mt-3 ">
                 <div class="text-center shadow py-3 border rounded">
                     <p class="capitalize text-base font-normal text-gray-800">income</p>
-                    <span class="text-green-500 text-base font-medium block pt-1">+$ {{ income() }}</span>
+                    <span class="text-green-500 text-base font-medium block pt-1">$ {{ positiveNumber }}</span>
                 </div>
                 <div class="text-center shadow py-3 border rounded">
                     <p class="capitalize text-base font-normal text-gray-800">expense</p>
-                    <span class="text-red-500 text-base font-medium block pt-1">-$</span>
+                    <span class="text-red-500 text-base font-medium block pt-1">$ {{ negativeNumber }} </span>
                 </div>
             </div>
             <p class="mt-5 text-gray-800 text-sm font-medium border-b pb-1">History</p>
             <div class="h-[110px] overflow-y-auto">
                 <div v-for="(item, index) in addItem" :key="index">
-                    <div class=" mt-2 mb-1 flex items-center py-1 px-3 bg-gray-100 relative group border-r-4 border-r-green-500">
-                        <img class="absolute -left-5 py-1 bg-red-50 z-0 invisible group-hover:z-10 group-hover:visible cursor-pointer" src="../svg/cross.svg">
+                    <div class="mt-2 ml-5 relative group mb-1 flex items-center py-1 px-3 bg-gray-100 border-r-4 " :class="item.amount > 0 ? 'bg-green-200 border-r-green-500' : 'bg-red-200 border-r-red-500'">
+                        <img @click="removeItem(item)" class="absolute z-0 invisible group-hover:visible group-hover:z-10 hover:bg-red-100 bg-red-50 -left-5 top-0 py-1 cursor-pointer" src="../svg/cross.svg">
                         <p class="text-sm font-medium text-gray-800 capitalize">{{ item.name }}</p> 
                         <h6 class="text-sm font-medium text-gray-800 ml-auto"> $ {{ item.amount }} </h6>
+                        
                     </div>
                 </div>
             </div>
